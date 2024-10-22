0bd7610fc33c7060ff7fd13f6d1b01f1d679cddb
Miguel David González del Toro <migueldgdt@estudiantes.uci.cu>
2024-10-22 13:50:25 -0400

    build(settings): created basic settings for Redis Cache

---
+++ Roadmap.WeatherAPI/Configuration/RedisSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Roadmap.WeatherAPI.Configuration
+{
+    public record RedisSettings
+    {
+        public required string ConnectionString { get; init; }
+        public required int DatabaseId { get; init; }
+        public required TimeSpan CacheExpiration { get; init; }
+    }
+}
